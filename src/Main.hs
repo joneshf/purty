20dc8d1237add1dea0bd09b9851cd0ff4a6ae2df
joneshf <jones3.hardy@gmail.com>
2018-03-31 18:34:57 -0700

    Support value declarations
    Probably could have split this into smaller chunks.
    But, you know how you just kind of get on a roll?
    
    There are some questionable printings here.
    The `Expr` contains some cases that a user cannot create.
    For instance, `TypeClassDictionaryConstructorApp`.
    In those cases, we don't print anything.
    We're also probably messing something up as far as operators go.
    Also, there are no where clauses?!?
    
    The `Expr` is just a smidge too dynamically typed for our case.
    Might want to reach out for some help/guidance.

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,24 @@
 module Main where
 
-import "protolude" Protolude
+import "protolude" Protolude hiding (group, list)
 
 import "base" Data.List                                      (span)
 import "prettyprinter" Data.Text.Prettyprint.Doc
     ( Doc
     , LayoutOptions
+    , comma
     , defaultLayoutOptions
     , enclose
+    , encloseSep
+    , flatAlt
+    , group
     , indent
     , layoutSmart
     , line
+    , list
     , parens
     , pretty
+    , punctuate
     , sep
     , tupled
     , vsep
@@ -20,28 +26,48 @@ import "prettyprinter" Data.Text.Prettyprint.Doc
     )
 import "prettyprinter" Data.Text.Prettyprint.Doc.Render.Text (renderIO)
 import "purescript" Language.PureScript
-    ( Comment(BlockComment, LineComment)
+    ( Binder
+    , CaseAlternative(CaseAlternative)
+    , Comment(BlockComment, LineComment)
     , DataDeclType(Data, Newtype)
-    , Declaration(DataBindingGroupDeclaration, DataDeclaration, ImportDeclaration, TypeDeclaration, TypeSynonymDeclaration)
+    , Declaration(DataBindingGroupDeclaration, DataDeclaration, ImportDeclaration, TypeDeclaration, TypeSynonymDeclaration, ValueDeclaration)
     , DeclarationRef(KindRef, ModuleRef, ReExportRef, TypeClassRef, TypeInstanceRef, TypeOpRef, TypeRef, ValueOpRef, ValueRef)
+    , DoNotationElement(DoNotationBind, DoNotationLet, DoNotationValue, PositionedDoNotationElement)
+    , Expr(Abs, Accessor, AnonymousArgument, App, BinaryNoParens, Case, Constructor, DeferredDictionary, Do, Hole, IfThenElse, Let, Literal, ObjectUpdate, ObjectUpdateNested, Op, Parens, PositionedValue, TypeClassDictionary, TypeClassDictionaryAccessor, TypeClassDictionaryConstructorApp, TypedValue, UnaryMinus, Var)
+    , Guard(ConditionGuard, PatternGuard)
+    , GuardedExpr(GuardedExpr)
     , ImportDeclarationType(Explicit, Hiding, Implicit)
     , Kind
+    , Literal(ArrayLiteral, BooleanLiteral, CharLiteral, NumericLiteral, ObjectLiteral, StringLiteral)
     , Module(Module)
     , ModuleName
+    , PathNode(Branch, Leaf)
+    , PathTree(PathTree)
     , ProperName
     , ProperNameType(ConstructorName)
     , TypeDeclarationData(TypeDeclarationData)
+    , ValueDeclarationData(ValueDeclarationData)
+    , caseAlternativeBinders
+    , caseAlternativeResult
     , isImportDecl
     , parseModuleFromFile
+    , prettyPrintBinder
     , prettyPrintKind
+    , prettyPrintString
     , prettyPrintType
+    , runAssocList
     , runIdent
     , runModuleName
     , runProperName
     , showOp
+    , showQualified
     , tydeclIdent
     , tydeclType
+    , valdeclBinders
+    , valdeclExpression
+    , valdeclIdent
     )
+import "purescript" Language.PureScript.PSString             (PSString)
 import "microlens-platform" Lens.Micro.Platform              (Lens', view)
 import "optparse-applicative" Options.Applicative
     ( Parser
@@ -72,9 +98,9 @@ main = do
   envArgs <- execParser argsInfo
   let envPrettyPrintConfig =
         PrettyPrintConfig { layoutOptions = defaultLayoutOptions }
-  runApp Env { envArgs, envPrettyPrintConfig } purty
+  runPurty Env { envArgs, envPrettyPrintConfig } purty
 
-purty :: (HasArgs env, HasPrettyPrintConfig env) => App env ()
+purty :: (HasArgs env, HasPrettyPrintConfig env) => Purty env ()
 purty = do
   Args { filePath } <- view argsL
   PrettyPrintConfig { layoutOptions } <- view prettyPrintConfigL
@@ -103,6 +129,15 @@ docFromModule (Module _ comments name declarations' exports) =
     <> line
   where
   (imports, declarations) = span isImportDecl declarations'
+
+docFromBinder :: Binder -> Doc a
+docFromBinder = pretty . prettyPrintBinder
+
+docFromCaseAlternative :: CaseAlternative -> Doc a
+docFromCaseAlternative CaseAlternative {caseAlternativeBinders, caseAlternativeResult} =
+  sep (punctuate comma $ map docFromBinder caseAlternativeBinders)
+    <> line
+    <> indent 2 (vsep $ map docFromGuardedExprCase caseAlternativeResult)
 
 docFromComment :: Comment -> Doc a
 docFromComment = \case
@@ -145,7 +180,23 @@ docFromDeclaration = \case
       <+> foldMap docFromParameter parameters
       <> line
       <> indent 2 ("=" <+> pretty (prettyPrintType underlyingType))
+  ValueDeclaration ValueDeclarationData { valdeclBinders, valdeclExpression, valdeclIdent } ->
+    pretty (runIdent valdeclIdent)
+      <+> sep (map docFromBinder valdeclBinders)
+      <> line
+      <> indent 2 (vsep $ map docFromGuardedExpr valdeclExpression)
+      <> line
   _ -> mempty
+
+docFromDoElement :: DoNotationElement -> Doc a
+docFromDoElement = \case
+  DoNotationBind binder expr ->
+    docFromBinder binder <+> "<-" <+> docFromExpr expr
+  DoNotationLet declarations ->
+    "let" <> line <> indent 4 (docFromDeclarations declarations)
+  DoNotationValue expr -> docFromExpr expr
+  PositionedDoNotationElement _ comments element ->
+    foldMap docFromComment comments <> docFromDoElement element
 
 docFromExports :: [DeclarationRef] -> Doc a
 docFromExports = tupled . map docFromExport
@@ -164,6 +215,82 @@ docFromExport = \case
   ValueRef _ ident -> pretty (runIdent ident)
   ValueOpRef _ name -> pretty (showOp name)
 
+docFromExpr :: Expr -> Doc a
+docFromExpr = \case
+  Abs binder expr ->
+    "\\" <> docFromBinder binder <+> "->" <> line <> indent 2 (docFromExpr expr)
+  Accessor key expr -> docFromExpr expr <> "." <> pretty (prettyPrintString key)
+  AnonymousArgument -> "_"
+  App expr1 expr2 -> docFromExpr expr1 <+> docFromExpr expr2
+  BinaryNoParens left op right ->
+    -- XXX: Verify this is correct.
+    docFromExpr left <+> docFromExpr op <+> docFromExpr right
+  Case exprs alternatives ->
+    "case"
+      <+> sep (punctuate comma $ map docFromExpr exprs)
+      <> line
+      <> indent 2 (vsep $ map docFromCaseAlternative alternatives)
+  Constructor name -> pretty (showQualified runProperName name)
+  DeferredDictionary _ _ -> mempty
+  Do elements ->
+    "do"
+      <> line
+      <> indent 2 (vsep $ map docFromDoElement elements)
+  Hole hole -> "?" <> pretty hole
+  IfThenElse b t f ->
+    "if"
+      <+> docFromExpr b
+      <+> "then"
+      <> line
+      <> indent 2 (docFromExpr t)
+      <> line
+      <> indent (-2) "else"
+      <> line
+      <> indent 2 (docFromExpr f)
+  Let declarations expr ->
+    "let"
+      <> line
+      <> indent 4 (vsep $ map docFromDeclaration declarations)
+      <> line
+      <> indent (-4) "in"
+      <> line
+      <> indent 4 (docFromExpr expr)
+  Literal literal -> docFromLiteral (map docFromExpr literal)
+  ObjectUpdate expr obj ->
+    docFromExpr expr <+> object (map (docFromObjectUpdate . map docFromExpr) obj)
+  ObjectUpdateNested expr pathTree ->
+    docFromExpr expr <+> docFromPathTree pathTree
+  Op op -> pretty (showQualified showOp op)
+  Parens expr -> parens (docFromExpr expr)
+  PositionedValue _ comments expr ->
+    foldMap docFromComment comments <> docFromExpr expr
+  TypeClassDictionary _ _ _ -> mempty
+  TypeClassDictionaryAccessor _ _ -> mempty
+  TypeClassDictionaryConstructorApp _ _ -> mempty
+  TypedValue _ expr exprType ->
+    docFromExpr expr <+> "::" <+> pretty (prettyPrintType exprType)
+  UnaryMinus expr -> "-" <> docFromExpr expr
+  Var ident -> pretty (showQualified runIdent ident)
+
+docFromGuard :: Guard -> Doc a
+docFromGuard = \case
+  ConditionGuard expr -> docFromExpr expr
+  PatternGuard binder expr -> docFromBinder binder <+> "<-" <+> docFromExpr expr
+
+docFromGuardedExpr :: GuardedExpr -> Doc a
+docFromGuardedExpr (GuardedExpr guards expr) =
+  sep (punctuate comma (map docFromGuard guards))
+    <+> "="
+    <> line
+    <> indent 2 (docFromExpr expr)
+
+docFromGuardedExprCase :: GuardedExpr -> Doc a
+docFromGuardedExprCase (GuardedExpr guards expr) =
+  sep (punctuate comma (map docFromGuard guards))
+    <+> "->"
+    <> line
+    <> indent 2 (docFromExpr expr)
+
 docFromConstructors :: [ProperName 'ConstructorName] -> Doc a
 docFromConstructors [] = mempty
 docFromConstructors constructors =
@@ -171,6 +298,16 @@ docFromConstructors constructors =
 
 docFromKind :: Kind -> Doc a
 docFromKind = pretty . prettyPrintKind
+
+docFromLiteral :: Literal (Doc a) -> Doc a
+docFromLiteral = \case
+  ArrayLiteral xs -> list xs
+  BooleanLiteral b -> pretty b
+  CharLiteral c -> pretty c
+  NumericLiteral (Left x) -> pretty x
+  NumericLiteral (Right x) -> pretty x
+  ObjectLiteral obj -> object (map docFromObject obj)
+  StringLiteral str -> pretty (prettyPrintString str)
 
 docFromImports :: [Declaration] -> Doc a
 docFromImports = vsep . map docFromImport
@@ -193,10 +330,29 @@ docFromImportType = \case
   Hiding declarationRefs -> "hiding" <+> docFromExports declarationRefs
   Implicit -> mempty
 
+docFromObject :: (PSString, Doc a) -> Doc a
+docFromObject (key, val) = pretty (prettyPrintString key) <> ":" <+> val
+
+docFromObjectUpdate :: (PSString, Doc a) -> Doc a
+docFromObjectUpdate (key, val) = pretty (prettyPrintString key) <+> "=" <+> val
+
 docFromParameter :: (Text, Maybe Kind) -> Doc a
 docFromParameter (parameter, Nothing) = pretty parameter
 docFromParameter (parameter, Just k) =
   parens (pretty parameter <+> "::" <+> docFromKind k)
+
+docFromPathTree :: PathTree Expr -> Doc a
+docFromPathTree (PathTree paths) =
+  object (map docFromPathNode $ runAssocList paths)
+
+docFromPathNode :: (PSString, PathNode Expr) -> Doc a
+docFromPathNode (key, Leaf expr) =
+  pretty (prettyPrintString key) <+> "=" <+> docFromExpr expr
+docFromPathNode (key, Branch path) =
+  pretty (prettyPrintString key) <+> docFromPathTree path
+
+object :: [Doc a] -> Doc a
+object = group . encloseSep (flatAlt "{ " "{") (flatAlt " }" "}") ", "
 
 data Args
   = Args
@@ -251,8 +407,8 @@ instance HasPrettyPrintConfig Env where
   prettyPrintConfigL f env = (\envPrettyPrintConfig -> env { envPrettyPrintConfig }) <$> f (envPrettyPrintConfig env)
 
 -- Locally defined rio since dependencies are wild.
-newtype App r a
-  = App { unApp :: ReaderT r IO a }
+newtype Purty r a
+  = Purty { unPurty :: ReaderT r IO a }
   deriving
     ( Applicative
     , Functor
@@ -261,5 +417,5 @@ newtype App r a
     , MonadReader r
     )
 
-runApp :: r -> App r a -> IO a
-runApp r (App x) = runReaderT x r
+runPurty :: r -> Purty r a -> IO a
+runPurty r (Purty x) = runReaderT x r
