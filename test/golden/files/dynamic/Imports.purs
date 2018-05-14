d1cbef811525415dc44430a113975448f1821990
joneshf <jones3.hardy@gmail.com>
2018-05-14 07:50:51 -0700

    Add failing tests for import sorting
    These aren't really failing now because the imports aren't sorted.
    It just prints out what you give it.
    We might think about having input files and output files for cases like this.

--- test/golden/files/dynamic/Imports.purs
+++ test/golden/files/dynamic/Imports.purs
@@ -3,8 +3,21 @@ module Foo where
 import Prelewd
 import Prelude
 
-import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Functor (class Functor, map, void)
 import Data.List (cons, nil)
-import Data.Maybe (maybe)
+import Data.Maybe (Maybe(Just, Nothing), maybe)
+import Prelude ( class Semiring
+               , Ordering(LT, EQ, GT)
+               , Void
+               , Unit
+               , compose
+               , class EuclideanRing
+               , one
+               , flap
+               , (<<<)
+               , (~>)
+               , (&&)
+               )
 
 import Data.List as List
--- test/golden/files/static/Imports.purs
+++ test/golden/files/static/Imports.purs
@@ -4,14 +4,34 @@ import Prelewd
 import Prelude
 
 import Control.Monad.Eff.Console
-  ( log
+  ( CONSOLE
+  , log
+  )
+import Data.Functor
+  ( class Functor
+  , map
+  , void
   )
 import Data.List
   ( cons
   , nil
   )
 import Data.Maybe
-  ( maybe
+  ( Maybe(Just, Nothing)
+  , maybe
+  )
+import Prelude
+  ( class Semiring
+  , Ordering(LT, EQ, GT)
+  , Void
+  , Unit
+  , compose
+  , class EuclideanRing
+  , one
+  , flap
+  , (<<<)
+  , (~>)
+  , (&&)
   )
 
 import Data.List as List
