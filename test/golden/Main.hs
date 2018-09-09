bc5884a7638d3b18f507e0c3e5f3d94fc3e33888
joneshf <jones3.hardy@gmail.com>
2018-09-09 11:09:49 -0700

    Check surjectivity of golden files
    We don't want to end up in a situation where we add a formatted file,
    and later realize it's not tested.
    That sounds like a frustrating experience.
    
    To nip that bud,
    we check that every formatted file has a corresponding original file.

--- test/golden/Main.hs
+++ test/golden/Main.hs
@@ -13,6 +13,7 @@ import "freer-simple" Control.Monad.Freer.Error
     , handleError
     )
 import "freer-simple" Control.Monad.Freer.Reader             (Reader, runReader)
+import "base" Data.Coerce                                    (coerce)
 import "freer-simple" Data.OpenUnion                         ((:++:))
 import "text" Data.Text.Lazy.Encoding                        (encodeUtf8)
 import "prettyprinter" Data.Text.Prettyprint.Doc             (LayoutOptions)
@@ -38,6 +39,11 @@ import "tasty" Test.Tasty
 import "tasty-golden" Test.Tasty.Golden
     ( findByExtension
     , goldenVsStringDiff
+    )
+import "tasty-hunit" Test.Tasty.HUnit
+    ( Assertion
+    , assertBool
+    , testCase
     )
 import "parsec" Text.Parsec                                  (ParseError)
 
@@ -69,6 +75,42 @@ diff old new = ["diff", "--unified", old, new]
 files :: Path Rel Dir
 files = [reldir|test/golden/files|]
 
+newtype FileName = FileName (Path Rel File)
+
+instance Eq FileName where
+  FileName x == FileName y = ((==) `on` filename) x y
+
+fileUsed :: (Foldable f) => f FileName -> FileName -> Assertion
+fileUsed haystack needle =
+  assertBool
+    ( toFilePath (coerce needle)
+    <> " is unused."
+    <> "Please add an original version to the `original` directory."
+    )
+    (needle `elem` haystack)
+
+filesUsedTests :: IO TestTree
+filesUsedTests = do
+  dynamics <- (fmap . fmap) coerce (psFiles [reldir|dynamic|])
+  originals <- (fmap . fmap) coerce (psFiles [reldir|original|])
+  statics <- (fmap . fmap) coerce (psFiles [reldir|static|])
+  pure $
+    testGroup
+      "filesUsed"
+      [ testCase "dynamic" (for_ dynamics $ fileUsed originals)
+      , testCase "static" (for_ statics $ fileUsed originals)
+      ]
+
+formattingTests :: IO TestTree
+formattingTests = do
+  originals <- psFiles [reldir|original|]
+  pure $
+    testGroup
+      "formatting"
+      [ testGroup "dynamic" (golden Dynamic <$> originals)
+      , testGroup "static" (golden Static <$> originals)
+      ]
+
 golden :: Formatting -> Path Rel File -> TestTree
 golden formatting originalFile =
   goldenVsStringDiff (toFilePath goldenFile) diff (toFilePath goldenFile) $ do
@@ -98,6 +140,11 @@ golden formatting originalFile =
     Static  -> [reldir|static|]
   goldenFile = files </> formattingDir </> filename originalFile
 
+psFiles :: Path Rel a -> IO [Path Rel File]
+psFiles dir = do
+  paths <- findByExtension [".purs"] (toFilePath $ files </> dir)
+  traverse parseRelFile paths
+
 test ::
   Path Abs File ->
   Eff
@@ -124,11 +171,11 @@ test absFile = encodeUtf8 . renderLazy <$> Purty.fromAbsFile absFile
 
 goldenTests :: IO TestTree
 goldenTests = do
-  paths <- findByExtension [".purs"] (toFilePath $ files </> [reldir|original|])
-  originals <- traverse parseRelFile paths
+  filesUsed <- filesUsedTests
+  formatting <- formattingTests
   pure $
     testGroup
       "golden"
-      [ testGroup "dynamic" (golden Dynamic <$> originals)
-      , testGroup "static" (golden Static <$> originals)
+      [ filesUsed
+      , formatting
       ]
