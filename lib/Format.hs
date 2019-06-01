84f941e959871177d9a477bcfda48744df782585
joneshf <jones3.hardy@gmail.com>
2019-05-31 19:38:21 -0700

    Fix some prefix stuff
    We weren't putting in some spaces properly.
    Given that the output is pretty raunchy,
    we might want to focus on annotating for a while.

--- lib/Format.hs
+++ lib/Format.hs
@@ -42,7 +42,8 @@ adoBlock log span indentation indent' adoBlock' = case adoBlock' of
         doStatements
       <> pure prefix
       <> sourceToken log indent blank in'
-      <> exprPrefix log indentation indent expr'
+      <> pure space
+      <> expr log indentation indent expr'
 
 blank :: Utf8Builder
 blank = ""
@@ -856,7 +857,40 @@ exprPrefix ::
   Indent ->
   Language.PureScript.CST.Expr Span.Span ->
   IO Utf8Builder
-exprPrefix = expr
+exprPrefix log indentation indent' expr' =
+    pure prefix
+      <> expr log indentation indent expr'
+  where
+  indent :: Utf8Builder
+  indent = indent' <> indentation
+
+  prefix :: Utf8Builder
+  prefix = case expr' of
+    Language.PureScript.CST.ExprAdo{} -> space
+    Language.PureScript.CST.ExprApp{} -> newline <> indent
+    Language.PureScript.CST.ExprArray{} -> newline <> indent
+    Language.PureScript.CST.ExprBoolean{} -> newline <> indent
+    Language.PureScript.CST.ExprCase{} -> space
+    Language.PureScript.CST.ExprChar{} -> newline <> indent
+    Language.PureScript.CST.ExprConstructor{} -> newline <> indent
+    Language.PureScript.CST.ExprDo{} -> space
+    Language.PureScript.CST.ExprHole{} -> newline <> indent
+    Language.PureScript.CST.ExprIdent{} -> newline <> indent
+    Language.PureScript.CST.ExprIf{} -> space
+    Language.PureScript.CST.ExprInfix{} -> newline <> indent
+    Language.PureScript.CST.ExprLambda{} -> space
+    Language.PureScript.CST.ExprLet{} -> space
+    Language.PureScript.CST.ExprNegate{} -> newline <> indent
+    Language.PureScript.CST.ExprNumber{} -> newline <> indent
+    Language.PureScript.CST.ExprOp{} -> newline <> indent
+    Language.PureScript.CST.ExprOpName{} -> newline <> indent
+    Language.PureScript.CST.ExprParens{} -> newline <> indent
+    Language.PureScript.CST.ExprRecord{} -> newline <> indent
+    Language.PureScript.CST.ExprRecordAccessor{} -> newline <> indent
+    Language.PureScript.CST.ExprRecordUpdate{} -> space
+    Language.PureScript.CST.ExprSection{} -> newline <> indent
+    Language.PureScript.CST.ExprString{} -> newline <> indent
+    Language.PureScript.CST.ExprTyped{} -> newline <> indent
 
 fixityFields ::
   Log.Handle ->
