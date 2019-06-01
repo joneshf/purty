331e08b5bdc34d15a16ae9f988d15c8a8a400c09
joneshf <jones3.hardy@gmail.com>
2019-05-31 19:39:21 -0700

    Unify printing of debug information
    Should make things a bit easier to deal with when we do more debugging.
    The debugging we do in formatting is all over the map.
    We should unify it there as well.

--- lib/Annotation.hs
+++ lib/Annotation.hs
@@ -9,33 +9,32 @@ import qualified "this" Log
 import qualified "this" Span
 
 dataMembers ::
+  (Show a) =>
   Log.Handle ->
   Language.PureScript.CST.DataMembers a ->
   IO (Language.PureScript.CST.DataMembers Span.Span)
 dataMembers log dataMembers' = case dataMembers' of
   Language.PureScript.CST.DataAll _ sourceToken' -> do
     let span = Span.SingleLine
-    Log.debug log ("Annotating `DataAll` as `" <> displayShow span <> "`")
+    debug log "DataAll" dataMembers' span
     pure (Language.PureScript.CST.DataAll span sourceToken')
   Language.PureScript.CST.DataEnumerated _ delimited' -> do
     let span = Span.dataMembers dataMembers'
-    Log.debug log ("Annotating `DataEnumerated` as `" <> displayShow span <> "`")
+    debug log "DataEnumerated" dataMembers' span
     pure (Language.PureScript.CST.DataEnumerated span delimited')
 
-debug ::
-  (Show a) =>
-  Utf8Builder ->
-  Language.PureScript.CST.Name a ->
-  Span.Span ->
-  Utf8Builder
-debug x y z =
-  "Annotating `"
-    <> x
-    <> "`: "
-    <> displayShow y
-    <> " as `"
-    <> displayShow z
-    <> "`"
+debug :: (Show a) => Log.Handle -> Utf8Builder -> a -> Span.Span -> IO ()
+debug log x y z =
+  Log.debug
+    log
+    ( "Annotating `"
+      <> x
+      <> "`: "
+      <> displayShow y
+      <> " as `"
+      <> displayShow z
+      <> "`"
+    )
 
 declaration ::
   Log.Handle ->
@@ -52,83 +51,87 @@ declaration log declaration' = do
   pure (span <$ declaration')
 
 export ::
+  (Show a) =>
   Log.Handle ->
   Language.PureScript.CST.Export a ->
   IO (Language.PureScript.CST.Export Span.Span)
 export log export' = case export' of
   Language.PureScript.CST.ExportClass _ class' name' -> do
-    Log.debug log (debug "ExportClass" name' span)
+    debug log "ExportClass" name' span
     pure (Language.PureScript.CST.ExportClass span class' name')
   Language.PureScript.CST.ExportKind _ kind' name' -> do
-    Log.debug log (debug "ExportKind" name' span)
+    debug log "ExportKind" name' span
     pure (Language.PureScript.CST.ExportKind span kind' name')
   Language.PureScript.CST.ExportModule _ module'' name' -> do
-    Log.debug log (debug "ExportModule" name' span)
+    debug log "ExportModule" name' span
     pure (Language.PureScript.CST.ExportModule span module'' name')
   Language.PureScript.CST.ExportOp _ name' -> do
-    Log.debug log (debug "ExportOp" name' span)
+    debug log "ExportOp" name' span
     pure (Language.PureScript.CST.ExportOp span name')
   Language.PureScript.CST.ExportType _ name' dataMembers'' -> do
-    Log.debug log (debug "ExportType" name' span)
+    debug log "ExportType" name' span
     dataMembers' <- traverse (dataMembers log) dataMembers''
     pure (Language.PureScript.CST.ExportType span name' dataMembers')
   Language.PureScript.CST.ExportTypeOp _ type'' name' -> do
-    Log.debug log (debug "ExportTypeOp" name' span)
+    debug log "ExportTypeOp" name' span
     pure (Language.PureScript.CST.ExportTypeOp span type'' name')
   Language.PureScript.CST.ExportValue _ name' -> do
-    Log.debug log (debug "ExportValue" name' span)
+    debug log "ExportValue" name' span
     pure (Language.PureScript.CST.ExportValue span name')
   where
   span :: Span.Span
   span = Span.export export'
 
 import' ::
+  (Show a) =>
   Log.Handle ->
   Language.PureScript.CST.Import a ->
   IO (Language.PureScript.CST.Import Span.Span)
 import' log import'' = case import'' of
   Language.PureScript.CST.ImportClass _ class' name' -> do
-    Log.debug log (debug "ImportClass" name' span)
+    debug log "ImportClass" name' span
     pure (Language.PureScript.CST.ImportClass span class' name')
   Language.PureScript.CST.ImportKind _ kind' name' -> do
-    Log.debug log (debug "ImportKind" name' span)
+    debug log "ImportKind" name' span
     pure (Language.PureScript.CST.ImportKind span kind' name')
   Language.PureScript.CST.ImportOp _ name' -> do
-    Log.debug log (debug "ImportOp" name' span)
+    debug log "ImportOp" name' span
     pure (Language.PureScript.CST.ImportOp span name')
   Language.PureScript.CST.ImportType _ name' dataMembers'' -> do
-    Log.debug log (debug "ImportType" name' span)
+    debug log "ImportType" name' span
     dataMembers' <- traverse (dataMembers log) dataMembers''
     pure (Language.PureScript.CST.ImportType span name' dataMembers')
   Language.PureScript.CST.ImportTypeOp _ type'' name' -> do
-    Log.debug log (debug "ImportTypeOp" name' span)
+    debug log "ImportTypeOp" name' span
     pure (Language.PureScript.CST.ImportTypeOp span type'' name')
   Language.PureScript.CST.ImportValue _ name' -> do
-    Log.debug log (debug "ImportValue" name' span)
+    debug log "ImportValue" name' span
     pure (Language.PureScript.CST.ImportValue span name')
   where
   span :: Span.Span
   span = Span.import' import''
 
 importDecl ::
+  (Show a) =>
   Log.Handle ->
   Language.PureScript.CST.ImportDecl a ->
   IO (Language.PureScript.CST.ImportDecl Span.Span)
 importDecl log importDecl' = case importDecl' of
   Language.PureScript.CST.ImportDecl _ import'' name' imports' rename -> do
     let span = Span.importDecl importDecl'
-    Log.debug log ("Annotating `ImportDecl` as `" <> displayShow span <> "`")
+    debug log "ImportDecl" importDecl' span
     imports <- (traverse . traverse . traverse . traverse) (import' log) imports'
     pure (Language.PureScript.CST.ImportDecl span import'' name' imports rename)
 
 module' ::
+  (Show a) =>
   Log.Handle ->
   Language.PureScript.CST.Module a ->
   IO (Language.PureScript.CST.Module Span.Span)
 module' log module''' = case module''' of
   Language.PureScript.CST.Module _ module'' name exports' where'' imports' declarations' trailing -> do
     let span = Span.betweenSourceTokens module'' where''
-    Log.debug log ("Annotating `Module` as `" <> displayShow span <> "`")
+    debug log "Module" module''' span
     exports <- (traverse . traverse . traverse) (export log) exports'
     imports <- traverse (importDecl log) imports'
     declarations <- traverse (declaration log) declarations'
