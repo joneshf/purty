8d3356c139bb1aba379bbf76ce48ce176b4bc6ef
joneshf <jones3.hardy@gmail.com>
2019-06-03 00:33:05 -0700

    Add a test for infix values retaining lines
    We want to support chains of infix values.
    At some point, it becomes easier to break them across lines.
    
    We ensure that we keep this behavior through the changes we make.

---
+++ test/golden/files/formatted/InfixBreaking.purs
@@ -0,0 +1,11 @@
+module InfixBreaking where
+
+someAppFunction =
+  functionA
+    <*> functionB
+    <*> functionC
+    <*> functionD
+    <*> functionE
+    <*> functionF
+
+another = a <*> b <*> c
