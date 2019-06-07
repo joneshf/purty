40d37052bcdb5bd465d5ff4360f936d5c7aff0aa
joneshf <jones3.hardy@gmail.com>
2019-06-07 06:38:57 -0700

    Add a failing test for multi-line records
    We are not indenting enough currently.
    We need them to go in a bit more.
    This might be a problem with only types, as the values seem to work.

--- test/golden/files/original/TypeSynonymNewline.purs
+++ test/golden/files/original/TypeSynonymNewline.purs
@@ -3,14 +3,14 @@ module TypeSynonymNewline where
 type T
   = {foo :: Int, bar :: {baz :: Int, qux :: {lhs :: Int, rhs :: Int}}}
 
-init :: T
-init =
-  { foo: 1
-  , bar:
-    { baz: 2
-    , qux:
-      { lhs: 3
-      , rhs: 4
-      }
-    }
-  }
+type U
+  = {foo :: Int, bar :: {baz :: Int, qux ::
+                            {lhs :: Int, rhs :: Int}}}
+
+t :: T
+t = { foo: 1 , bar: { baz: 2 , qux: { lhs: 3 , rhs: 4}}}
+
+u :: T
+u =
+  { foo: 1 , bar: { baz: 2 , qux:
+      { lhs: 3 , rhs: 4}}}
