2f970e3d55b6218339b4af5f713a526866d3068f
joneshf <jones3.hardy@gmail.com>
2019-06-24 05:57:31 -0700

    Add failing test for paren indentation
    We're not indenting enough for any constructs that are parenthesized.
    We add a failing test to validate those failures.

---
+++ test/golden/files/formatted/ParenthesesIndentation.purs
@@ -0,0 +1,41 @@
+module ParenthesesIndentation where
+
+foreign import kind Foo
+
+foreign import data Bar ::
+  ( Foo ->
+    Foo
+  )
+
+instance bazQux ::
+  ( Baz a
+  , Baz
+      b
+  ) =>
+  Baz (Qux a b)
+
+foo ::
+  ( Array
+      Int
+  )
+foo =
+  ( [ 1
+    , 2
+    , 3
+    , 4
+    ]
+  )
+
+bar ::
+  Either (Int -> Int)
+    ( Int ->
+      Int
+    ) ->
+  Int -> Int
+bar x = case x of
+  ( Left
+      y
+  ) -> y
+  ( Right
+      z
+  ) -> z
