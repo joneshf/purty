659383239be034a6088a8c8b95dd92083ce0133b
Gabe Johnson <gijohnson105@gmail.com>
2019-07-15 10:03:20 -0500

    Address PR comments

--- purty.el
+++ purty.el
@@ -1,34 +1,34 @@
-;;; purty-format.el --- Minor-mode to format PureScript code with purty on save
+;;; purty.el --- Minor-mode to format PureScript code with purty on save
 
 ;; Version: 0.1.0
 ;; Keywords: purescript format purty
 ;; Package-Requires: ((reformatter "0.4"))
-;; URL: https://gitlab.com/joneshf/purty/purty-format.el
+;; URL: https://gitlab.com/joneshf/purty
 ;; Author: Gabe Johnson
 
 ;; To use this package with `purescript-mode', simply load the file and add a
 ;; directory local variable in a `.dir-locals.el' file at the root of your
 ;; project:
 ;;
-;;   ((purescript-mode (mode . purty-format-on-save)))
+;;   ((purescript-mode (mode . purty-on-save)))
 
 (require 'reformatter)
 
-(defgroup purty-format nil
+(defgroup purty nil
   "Minor mode for formatting PureScript buffers with purty."
   :group 'languages
-  :link '(url-link "https://gitlab.com/joneshf/purty/purty-format.el"))
+  :link '(url-link "https://gitlab.com/joneshf/purty"))
 
-(defcustom purty-format-command "purty"
-  "The name of the `purty-format` command."
-  :group 'purty-format
+(defcustom purty-command "purty"
+  "The name of the `purty` command."
+  :group 'purty
   :type 'string)
 
-(reformatter-define purty-format
-  :program purty-format-command
+(reformatter-define purty
+  :program purty-command
   :args '("-")
-  :group 'purty-format
+  :group 'purty
   :lighter " PurtyFmt")
 
-(provide 'purty-format)
-;;; purty-format.el ends here
+(provide 'purty)
+;;; purty.el ends here
