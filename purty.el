2f5e487b735f50084296b0069212f29584140d80
Gabe Johnson <gijohnson105@gmail.com>
2019-08-06 03:04:32 +0000

    Allow the user to use a locally installed binary

--- purty.el
+++ purty.el
@@ -12,6 +12,9 @@
 ;;
 ;;   ((purescript-mode (mode . purty-on-save)))
 
+;; If you would prefer to install `purty' locally using `npm' or `yarn', set the
+;; `purty-use-npm-bin' to true.
+
 (require 'reformatter)
 
 (defgroup purty nil
@@ -25,10 +28,30 @@
   :type 'string)
 
 (reformatter-define purty
-  :program purty-command
+  :program (purty-executable-path)
   :args '("-")
   :group 'purty
   :lighter " PurtyFmt")
+
+;; The following is adapted from https://github.com/purescript-emacs/psc-ide-emacs/blob/a10cc85565f330ee277698b27f3f715fef2e1ce2/psc-ide.el
+
+(defcustom purty-use-npm-bin nil
+  "Whether to use `npm bin` to determine the location of purty"
+  :group 'purty
+  :type 'boolean)
+
+(defun purty-executable-path ()
+  "Return the full path to the purty executable."
+  (let* ((npm-bin-path (if purty-use-npm-bin
+                           (purty-npm-bin-executable purty-command)
+                         nil)))
+    (or npm-bin-path purty-command)))
+
+(defun purty-npm-bin-executable (cmd)
+  "Find purty binary CMD of current project by invoking `npm bin`."
+  (let* ((npm-bin (s-trim-right (shell-command-to-string "npm bin")))
+         (formatter (expand-file-name cmd npm-bin)))
+    (if (and formatter (file-executable-p formatter)) formatter nil)))
 
 (provide 'purty)
 ;;; purty.el ends here
